e5e3704bbbda6007b26a81c7f850f51d136a0013
RAZA KHAN <140490835+Razakhan143@users.noreply.github.com>
2025-01-20 22:38:45 +0500

    add selection bar in app

--- Data Analysis/Analysis on olympic/app.py
+++ Data Analysis/Analysis on olympic/app.py
@@ -13,8 +13,16 @@ user_menu=st.sidebar.radio(
     
 )
 if user_menu == 'Medal Tally':
+    st.header("Medal Tally")
+    years,country = helper.country_year_list(df)
+    
+    selected_year=st.sidebar.selectbox("Select Year",years)
+    selected_country=st.sidebar.selectbox("Select Country",country)
+    
     medal_tally = helper.medal_tally(df)
     st.dataframe(medal_tally)
+    
+    
     
 #for running the app
 #streamlit run "D:\PROFESSIONAL\AI\Data Science\Data Analysis\Analysis on olympic\app.py"
