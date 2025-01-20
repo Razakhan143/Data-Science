b8c70144c3b924daa9a874ec6b612f89c081251b
RAZA KHAN <140490835+Razakhan143@users.noreply.github.com>
2025-01-20 22:33:41 +0500

    add new file and init app

---
+++ Data Analysis/Analysis on olympic/Preprocessor.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def preprocessor(df,region_df):
+    
+    df = df[df['Season']=='Summer']
+    df=df.merge(region_df,on="NOC",how='left')
+    df.drop_duplicates(inplace=True)
+    df=pd.concat([df,pd.get_dummies(df['Medal']).astype('int8')],axis=1)
+    return df
--- Data Analysis/Analysis on olympic/app.py
+++ Data Analysis/Analysis on olympic/app.py
@@ -1,9 +1,20 @@
 import streamlit as st
 import pandas as pd
+import Preprocessor,helper
+df=pd.read_csv(r'D:\PROFESSIONAL\AI\Data Science\Data Analysis\Analysis on olympic\dataset\Data_set\Athlete_events.csv')
+region_df=pd.read_csv(r"D:\PROFESSIONAL\AI\Data Science\Data Analysis\Analysis on olympic\dataset\Data_set\noc_regions.csv")
 
-st.slidebar.radio(
+
+df = Preprocessor.preprocessor(df,region_df)
+
+user_menu=st.sidebar.radio(
     'Select an option',
     ('Medal Tally','overall Analysis','Country-Wise Analysis','Athlete-wise Analysis')
     
 )
-git push -u origin master
+if user_menu == 'Medal Tally':
+    medal_tally = helper.medal_tally(df)
+    st.dataframe(medal_tally)
+    
+#for running the app
+#streamlit run "D:\PROFESSIONAL\AI\Data Science\Data Analysis\Analysis on olympic\app.py"
