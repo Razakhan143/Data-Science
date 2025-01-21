26f62b62142bcf4dea56d6dc76a759afd34243b4
RAZA KHAN <140490835+Razakhan143@users.noreply.github.com>
2025-01-21 14:01:06 +0500

    add new features

--- Data Analysis/Analysis on olympic/app.py
+++ Data Analysis/Analysis on olympic/app.py
@@ -11,7 +11,7 @@ st.sidebar.title("Olympics Analysis")
 
 user_menu=st.sidebar.radio(
     'Select an option',
-    ('Medal Tally','overall Analysis','Country-Wise Analysis','Athlete-wise Analysis')
+    ('Medal Tally','Overall Analysis','Country-Wise Analysis','Athlete-wise Analysis')
     
 )
 if user_menu == 'Medal Tally':
@@ -25,14 +25,53 @@ if user_menu == 'Medal Tally':
     
     if selected_year =='Overall' and selected_country == 'Overall':
         st.title('Overall Tally')
-    if selected_year =='Overall' and selected_country != 'Overall':
-        st.title('Medal Tally in '+ str(selected_year) + 'in Olympics')
+        
     if selected_year !='Overall' and selected_country == 'Overall':
-        st.title(selected_year+ 'Overall performance')
+        st.title('Medal Tally in '+ str(selected_year) + ' in Olympics')
+        
+    if selected_year =='Overall' and selected_country != 'Overall':
+        st.title(selected_country + ' Overall performance')
+        
     if selected_year !='Overall' and selected_country != 'Overall':
-        st.title(selected_country+'Performance in' + str(selected_year)+'Olympics')
+        st.title(selected_country+' Performance in ' + str(selected_year)+' Olympics')
     
     st.table(medal_tally)
+    
+if user_menu == "Overall Analysis":
+    editions = df['Year'].unique().shape[0]-1
+    cities = df['City'].unique()[0]
+    sports = df['Sport'].unique()[0]
+    events = df['Event'].unique().shape[0]
+    athletes = df['Name'].unique().shape[0]
+    nations = df['region'].unique().shape[0]
+    
+    st.title('Top Statistics')
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        st.header('Editions')
+        st.title(editions)
+        
+    with col2:
+        st.header('Hosts')
+        st.title(cities)
+        
+    with col3:
+        st.header('Sports')
+        st.title(sports)
+        
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        st.header('Events')
+        st.title(events)
+        
+    with col2:
+        st.header('Nations')
+        st.title(nations)
+        
+    with col3:
+        st.header('Athletes')
+        st.title(athletes)
+            
     
     
     
