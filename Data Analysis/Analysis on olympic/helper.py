bc9b3f244dc4584991a1f5d44b020f1cd307f1f8
RAZA KHAN <140490835+Razakhan143@users.noreply.github.com>
2025-01-21 20:52:04 +0500

    add contrwise medal tally graph

--- Data Analysis/Analysis on olympic/helper.py
+++ Data Analysis/Analysis on olympic/helper.py
@@ -50,3 +50,9 @@ def most_successful(df,sport):
     x.rename(columns={'count':'Medals'},inplace=True)
     return x   
     
+def year_wise_medal_tally(df,country):
+    temp_df = df.dropna(subset=['Medal'])
+    temp_df.drop_duplicates(subset=['Team','NOC','Games','Year','City','Sport','Event','Medal'],inplace=True)
+    temp_df=temp_df[temp_df['region']==country]
+    final_df=temp_df.groupby('Year').count()['Medal'].reset_index()
+    return final_df
